054e05a95b0974fee02678dae8e9a30ba5e548f3
tool root <smart.dog.lilienthal@gmail.com>
2017-08-01 09:32:57 +0900

    メッセージ送信実装、存在しないファイル名入力時に発生したバグ修正

--- CookieSound2/CookieSound2.py
+++ CookieSound2/CookieSound2.py
@@ -4,7 +4,7 @@ import re
 import os
 import glob
 import threading
-#Read config.ini
+#TODO Read config.ini
 import configparser
 
 from pygame import mixer
@@ -15,7 +15,7 @@ def irc_conn():
 
 #simple function to send data through the socket
 def send_data(command):
-    status = IRC.send((command + "\r\n").encode(CHARCODE)) # unicode -> iso2022_jp
+    status = IRC.send(bytes((command + "\r\n").encode(CHARCODE))) # unicode -> iso2022_jp
     print(command)
     if status == -1 :raise Exception('send_data error', status)
 
@@ -28,21 +28,27 @@ def login(nickname, username='user', password = None, realname='Marina', hostnam
     send_data("USER %s %s %s %s" % (username, hostname, servername, realname))
     send_data("NICK " + nickname)
 
+def send_msg(msg): # sends messages to the target.
+    IRC.send(bytes("PRIVMSG "+ CHANNEL +" :"+ msg +"\n", CHARCODE))
+
 def wait_connection():
     while(1):
         buffer = IRC.recv(1024)
         msg = str.split(str(buffer))
         if msg[3] == ":Welcome" and msg[2] == NICKNAME: #TODO splash screen
-            send_data("ze")
             break
 
 def keyhook():
     while(1):
-        str = input("Input:")
+        str = input(NICKNAME + ":")
         if str in OGGlist: 
-            send_data(str)
-        elif str in list:
-            send_data(str)
+            send_msg(str)
+            sound = mixer.Sound("sound\\" + str + ".ogg")
+            sound.play()
+        elif str in MP3list:
+            send_msg(str)
+            mixer.music.load("sound\\csr\\" + str + ".mp3")
+            mixer.music.play()
 
 if __name__ == '__main__':
     #some user data, change as per your taste
@@ -83,12 +89,16 @@ if __name__ == '__main__':
         if msg[0] == "PING":                #check if server have sent ping command
             send_data("PONG %s" % msg[1])   #answer with pong as per RFC 1459
         if msg[1] == 'PRIVMSG':
+            tmp_sname = re.sub(r"^b':", "", msg[0])
+            send_name = tmp_sname.split("!")
             tmp_fname = re.sub(r"^:", "", msg[3])
             filename = re.sub(r"\\r\\n'$", "", tmp_fname)
             if filename in OGGlist: 
+                print(send_name[0] + ":" + filename)
                 sound = mixer.Sound("sound\\" + filename + ".ogg")
                 sound.play()
             elif filename in MP3list:
+                print(send_name[0] + ":" + filename)
                 mixer.music.load("sound\\csr\\" + filename + ".mp3")
                 mixer.music.play()
 
