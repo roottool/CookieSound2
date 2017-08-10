2cf17bb7b4d95d90366cdac6a56d0d8ea4f85120
tool root <smart.dog.lilienthal@gmail.com>
2017-08-10 17:54:59 +0900

    不要だったkeyhookの無限ループを削除、soundオブジェクトをローカル変数化

--- CookieSound2/CookieSound2.py
+++ CookieSound2/CookieSound2.py
@@ -16,11 +16,10 @@ from mainform import Ui_Form
 
 isInput = False
 str_input = ''
-sound = None
 
 
 class MainForm(QDialog):
-    def __init__(self,parent=None):
+    def __init__(self, parent = None):
         super(MainForm, self).__init__(parent)
         self.ui = Ui_Form()
         self.setWindowFlags(Qt.FramelessWindowHint)
@@ -35,25 +34,26 @@ class MainForm(QDialog):
             self.ui.InputLabel.setText("Waiting for typing hookkey")
 
 
-#open a connection with the server
+# open a connection with the server
 def irc_conn():
     IRC.connect((SERVER, PORT))
 
-#send login data (customizable)
+# send login data (customizable)
 def login(nickname, username='user', password = None, realname='Marina', hostname='Helena', servername='Server'):
     send_data('USER %s %s %s %s' % (username, hostname, servername, realname))
     send_data('NICK ' + nickname)
 
-#join the channel
+# join the channel
 def join(channel):
     send_data('JOIN %s' % channel)
 
-#simple function to send data through the socket
+# simple function to send data through the socket
 def send_data(command):
     status = IRC.send(bytes((command + '\r\n').encode(CHARCODE))) # unicode -> iso2022_jp
     if status == -1 :raise Exception('send_data error', status)
 
-def send_msg(msg): # sends messages to the target.
+# sends messages to the target.
+def send_msg(msg): 
     IRC.send(bytes('PRIVMSG '+ CHANNEL +' :'+ msg +'\n', CHARCODE))
 
 def wait_connection(volume):
@@ -69,8 +69,8 @@ def receive(volume, soundvolume, OGGlist, MP3list):
     while True:
         buffer = IRC.recv(1024)
         msg = str.split(str(buffer))
-        if msg[0] == "b'PING":                #check if server have sent ping command
-            send_data('PONG %s' % msg[1])   #answer with pong as per RFC 1459
+        if msg[0] == "b'PING":                  # check if server have sent ping command
+            send_data('PONG %s' % msg[1])       # answer with pong as per RFC 1459
         if msg[1] == 'PRIVMSG':
             tmp_sname = re.sub(r"^b':", '', msg[0])
             send_name = tmp_sname.split('!')
@@ -80,39 +80,33 @@ def receive(volume, soundvolume, OGGlist, MP3list):
                 
                 
 def keyhook(volume, soundvolume, OGGlist, MP3list):
-    while True:
-        hk = Hook()                 # make a new instance of PyHooked
-        hk.handler = handle_events  # add a new shortcut ctrl+a, or triggered on mouseover of (300,400)
-        hk.hook()                   # hook into the events, and listen to the presses
+    hk = Hook()                 # make a new instance of PyHooked
+    hk.handler = handle_events  # add a new shortcut ctrl+a, or triggered on mouseover of (300,400)
+    hk.hook()                   # hook into the events, and listen to the presses
 
 def handle_events(args):
     global isInput
     global str_input
-    global sound
 
     if isinstance(args, KeyboardEvent):
         if isInput == True and args.current_key == 'Return' and args.event_type == 'key up':
             isInput = False
-            if str_input == 'stop':
-                mixer.music.stop()
-                sound.stop()
-            else:
-                play(NICKNAME, str_input, soundvolume)
+            play(NICKNAME, str_input, soundvolume)
             str_input = ''
-        elif isInput == True and args.current_key == 'Back' and args.event_type == 'key up':
+        elif isInput is True and args.current_key == 'Back' and args.event_type == 'key up':
             str_input = str_input[:-1]
-        elif isInput == True and (65 <= args.key_code and args.key_code <= 90) and args.event_type == 'key up':
+        elif isInput is True and (65 <= args.key_code <= 90) and args.event_type == 'key up':
             str_input = str_input + args.current_key.lower()
-        elif isInput == True and (48 <= args.key_code and args.key_code <= 57) and args.event_type == 'key up':
+        elif isInput is True and (48 <= args.key_code <= 57) and args.event_type == 'key up':
             str_input = str_input + args.current_key
-        elif isInput == True and (96 <= args.key_code and args.key_code <= 105) and args.event_type == 'key up':
+        elif isInput is True and (96 <= args.key_code <= 105) and args.event_type == 'key up':
             str_input += args.current_key[-1]
-        elif isInput == True and args.current_key == 'Oem_Minus' and args.event_type == 'key up':
+        elif isInput is True and args.current_key == 'Oem_Minus' and args.event_type == 'key up':
             str_input += '-'
-        elif isInput == True and args.current_key == 'Oem_7' and args.event_type == 'key up':
+        elif isInput is True and args.current_key == 'Oem_7' and args.event_type == 'key up':
             str_input += '^'
   
-        if isInput == False and args.current_key == HOOKKEY.upper() and args.event_type == 'key up':
+        if isInput is False and args.current_key == HOOKKEY.upper() and args.event_type == 'key up':
             isInput = True
             play(None, HOOKSOUND, 0)
         window.statsCheck()
@@ -120,9 +114,11 @@ def handle_events(args):
 
 def play(name, command, soundvolume):
     global log
-    global sound
 
-    if command in OGGlist: 
+    if command == 'stop':
+        mixer.quit()
+        mixer.init()
+    elif command in OGGlist: 
         if name != None:
             print(name + ':' + command)
         if os.path.exists(OGGlist[command]):
@@ -199,5 +195,5 @@ if __name__ == '__main__':
     
     window.show()
     splash.finish(window)
-    app.exec_()
-    sys.exit()
+    
+    sys.exit(app.exec_())
